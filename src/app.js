7f3080510feb4f00be384f2484be2eb8f229b03c
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-06-26 21:53:11 +0300

    fix: attempt to fix cpu usage & enhance preview

--- src/app.js
+++ src/app.js
@@ -12,14 +12,35 @@ function initSystemInfo() {
 
 function getCpuUsage() {
     chrome.system.cpu.getInfo(function (cpuInfo) {
-        const cpuStatsDiv = document.getElementById("cpuContent");
-        if (cpuStatsDiv) {
-            const cpuInfoHolder = document.createElement("p");
-            cpuInfoHolder.textContent = `${cpuInfo.archName} \r\n ${cpuInfo.modelName}`;
-            cpuStatsDiv.appendChild(cpuInfoHolder)
-            cpuStatsDiv.classList.add("fs-5");
+        if (!cpuInfo) {
+            console.error("Failed to get CPU information.");
+            return;
         } else {
-            console.error("CPU content element not found.");
+            const cpuStatsDiv = document.getElementById("cpuContent");
+            const cpuArchitectureDiv = document.getElementById("cpuArchitecture");
+
+            if (cpuStatsDiv) {
+                const cpuInfoHolder = document.createElement("p");
+                const cpuArchitectureInfoHolder = document.createElement("p");
+
+                cpuInfoHolder.setAttribute("class", "my-0")
+                cpuArchitectureInfoHolder.setAttribute("class", "my-0")
+
+                cpuInfoHolder.textContent = `${cpuInfo.modelName}`;
+                cpuArchitectureInfoHolder.textContent = `${cpuInfo.archName}`;
+
+                if (cpuInfo.archName === "x86_64" || cpuInfo.archName === "x86") {
+                    insertArchitectureIntel(cpuArchitectureDiv);
+                } else if (!cpuInfo.modelName.includes("Intel(R)" && cpuInfo.includes("AMD"))) {
+                    insertArchitectureAmd(cpuArchitectureDiv);
+                } else if (cpuInfo.archName === "arm64" || cpuInfo.archName === "arm") {
+                    insertArchitectureArm(cpuArchitectureDiv)
+                }
+
+                cpuStatsDiv.appendChild(cpuInfoHolder);
+                cpuStatsDiv.classList.add("fs-5");
+                cpuArchitectureDiv.appendChild(cpuArchitectureInfoHolder);
+            }
         }
     });
 }
@@ -73,4 +94,28 @@ function getStorageUsage() {
 
 function convertBytesToGb(bytes) {
     return (bytes / (1024 * 1024 * 1024)).toFixed(2);
+}
+
+function insertArchitectureIntel(divName) {
+    const intel = document.createElement("img");
+    intel.setAttribute("class", "img-fluid object-fit-cover rounded-3 mx-0 my-0");
+    intel.setAttribute("style", "width: 64px !important;")
+    intel.setAttribute("src", "/images/intel.png");
+    divName.appendChild(intel);
+}
+
+function insertArchitectureAmd(divName) {
+    const amd = document.createElement("img");
+    amd.setAttribute("class", "img-fluid object-fit-cover rounded-3 mx-0 my-0");
+    amd.setAttribute("style", "width: 64px !important;")
+    amd.setAttribute("src", "/images/amd.png");
+    divName.appendChild(amd);
+}
+
+function insertArchitectureArm(divName) {
+    const arm = document.createElement("img");
+    arm.setAttribute("class", "img-fluid object-fit-cover rounded-3 mx-0 my-0");
+    arm.setAttribute("style", "width: 64px !important;")
+    arm.setAttribute("src", "/images/arm.png");
+    divName.appendChild(arm);
 }
