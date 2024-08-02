305c155eb9f4cb0327725f98888582e57fc62848
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-08-02 16:52:38 +0300

    Feat: Error div shows an image if unsupported.

--- src/background/service_worker.js
+++ src/background/service_worker.js
@@ -1,34 +1,42 @@
 // Check if the current OS is ChromeOS or not
 chrome.runtime.getPlatformInfo((info) => {
-    if (info.os !== "cros") {
+    if (info.os !== "cros" && info.os !== "win") {
         preventExtensionRunning();
     } else {
         return console.log("Extension is running in ChromeOS.")
     }
 });
 
-let bannerDiv = document.getElementById("banner");
-let bannerHolderDiv = document.getElementById("bannerContent");
-let usageIndicatorDiv = document.getElementById("hardwareInfoDiv");
+const documentElements = {
+    bannerDiv: document.getElementById("banner"),
+    bannerHolderDiv: document.getElementById("bannerContent"),
+    usageIndicatorDiv: document.getElementById("hardwareInfoDiv")
+}
 
 function preventExtensionRunning() {
+    const textInfo = {
+        errorMessage: "This extension is only available on;",
+        errorDivClassValue: "px-2 py-5 text-center pt-6 text-white mx-0 my-0",
+        bannerDivBgColor: "bg-black text-white",
+        bannerDivImg: "img-fluid m-auto pb-4"
+    };
+
     const errorDiv = document.createElement("h1");
+    const errorDivImg = document.createElement("img");
+    errorDivImg.setAttribute("class", textInfo.bannerDivImg);
+    errorDivImg.src = "/images/cros-logo.png";
 
-    if (bannerDiv) {
-        const textInfo = {
-            errorMessage: "This extension is only available on ChromeOS.",
-            errorDivClassValue: "px-2 py-5 text-center pt-6 text-white mx-0 my-0",
-            bannerDivBgColor: "bg-danger",
-        };
-
-        bannerDiv.setAttribute("class", textInfo.bannerDivBgColor);
+    if (documentElements.bannerDiv) {
+        documentElements.bannerDiv.setAttribute("class", textInfo.bannerDivBgColor);
         errorDiv.setAttribute("class", textInfo.errorDivClassValue);
 
         errorDiv.textContent = textInfo.errorMessage;
 
-        bannerDiv.appendChild(errorDiv);
-        bannerHolderDiv.remove();
-        usageIndicatorDiv.remove();
+        documentElements.bannerDiv.appendChild(errorDiv);
+        documentElements.bannerDiv.appendChild(errorDivImg);
+
+        documentElements.bannerHolderDiv.remove();
+        documentElements.usageIndicatorDiv.remove();
     }
     return console.error(textInfo.errorMessage);
 }
