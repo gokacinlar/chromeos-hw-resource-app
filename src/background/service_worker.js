24fb8ebcfb46da176557388c9b174ff2785009e2
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-09-16 13:30:45 +0300

    Refactor service_worker.js

--- src/background/service_worker.js
+++ src/background/service_worker.js
@@ -1,56 +1,72 @@
 // Check if the current OS is ChromeOS or not
-chrome.runtime.getPlatformInfo((info) => {
+chrome.runtime.getPlatformInfo(info => {
     if (info.os !== "cros") {
         preventExtensionRunning();
     } else {
-        return console.log("Extension is running in ChromeOS.")
+        console.log("Extension is running in ChromeOS.");
     }
 });
 
+// Define DOM elements
 const documentElements = {
     bannerDiv: document.getElementById("banner")
-}
+};
 
-const elemsToBeRemove = {
+const elemsToBeRemoved = {
     bannerHolderDiv: document.getElementById("bannerContent"),
     usageIndicatorDiv: document.getElementById("hardwareInfoDiv"),
     usageStatsDiv: document.getElementById("hardwareMonitoringDiv"),
     tabsDiv: document.getElementById("tabsDiv")
-}
+};
 
+// Function to prevent user from running the extension if OS is not intended to run it
 function preventExtensionRunning() {
     const textInfo = {
-        errorMessage: "This extension is only available on;",
-        errorDivClassValue: "px-2 py-5 text-center pt-6 text-white mx-0 my-0",
-        bannerDivBgColor: "bg-black text-white",
-        bannerDivImg: "img-fluid m-auto pb-4"
+        errorMessage: "This extension is only available on ChromeOS.",
+        errorDivClass: "px-2 py-5 text-center pt-6 text-white mx-0 my-0",
+        bannerDivClass: "bg-black text-white",
+        bannerDivImgClass: "img-fluid m-auto pb-4",
+        bannerDivImgSrc: "/images/cros-logo.png"
     };
 
-    const errorDiv = document.createElement("h1");
-    const errorDivImg = document.createElement("img");
-    errorDivImg.setAttribute("class", textInfo.bannerDivImg);
-    errorDivImg.src = "/images/cros-logo.png";
-
     if (documentElements.bannerDiv) {
-        documentElements.bannerDiv.setAttribute("class", textInfo.bannerDivBgColor);
-        errorDiv.setAttribute("class", textInfo.errorDivClassValue);
+        documentElements.bannerDiv.className = textInfo.bannerDivClass;
 
-        errorDiv.textContent = textInfo.errorMessage;
-
-        for (let key in elemsToBeRemove) {
-            if (elemsToBeRemove.hasOwnProperty(key)) {
-                removeElements(elemsToBeRemove[key]);
-            }
-        }
-
+        // Create and append error message
+        const errorDiv = createErrorElement(textInfo);
         documentElements.bannerDiv.appendChild(errorDiv);
-        documentElements.bannerDiv.appendChild(errorDivImg);
+
+        Object.values(elemsToBeRemoved).forEach(removeElement);
+
+        console.error(textInfo.errorMessage);
+    } else {
+        console.error("Banner div not found.");
     }
-    return console.error(textInfo.errorMessage);
 }
 
-function removeElements(elem) {
+// Function to create an error element to display error state
+function createErrorElement({ errorMessage, errorDivClass, bannerDivImgClass, bannerDivImgSrc }) {
+    const errorDiv = document.createElement("h1");
+    errorDiv.className = errorDivClass;
+    errorDiv.textContent = errorMessage;
+
+    const errorDivImg = document.createElement("img");
+    errorDivImg.className = bannerDivImgClass;
+    errorDivImg.src = bannerDivImgSrc;
+
+    // Append image to error div
+    errorDiv.appendChild(errorDivImg);
+    return errorDiv;
+}
+
+/**
+ * Helper Functions
+ */
+
+function removeElement(elem) {
     if (elem) {
         elem.remove();
+    } else {
+        console.error("Provided element could not be found.");
     }
 }
